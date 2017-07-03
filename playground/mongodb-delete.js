0967ff6b0848420d1bc75b9e62bf41f7dc611d23
shubhamsapra97 <shubham.sapra97@gmail.com>
2017-07-03 23:43:39 +0530

    Add delete  script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,35 @@
+var {MongoClient , ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp' , (err,db)=>{
+  if(err){
+    return console.log('Unable to connect to Mongodb Server');
+  }
+  console.log('Connected to MongoDb Server');
+
+  //deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((docs)=>{
+  //   console.log(docs);
+  // });
+
+  //deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat unch'}).then((result)=>{
+  //   console.log(result);
+  // });
+
+  //findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result)=>{
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Shubham'}).then((result)=>{
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID("595a306c9a165027b44d9962")
+  }).then((result)=>{
+    console.log(result);
+  });
+
+  db.close();
+});
